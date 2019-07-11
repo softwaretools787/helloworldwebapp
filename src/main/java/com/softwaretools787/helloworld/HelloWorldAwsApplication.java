047e880dae60dacf63a195e0cb676c267dd2f28d
Surya Vellore (sv231k) <sv231k@att.com>
2019-07-11 14:34:42 -0700

    Initial Changes for Spring boot application

---
+++ src/main/java/com/softwaretools787/helloworld/HelloWorldAwsApplication.java
@@ -0,0 +1,13 @@
+package com.softwaretools787.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldAwsApplication.class, args);
+	}
+
+}
